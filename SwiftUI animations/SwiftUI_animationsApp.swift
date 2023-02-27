3f290714a2cf1b2b53d92619fd2490f8116fcb6c
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-02-27 15:42:32 +0800

    Initial Commit

---
+++ SwiftUI animations/SwiftUI_animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_animationsApp.swift
+//  SwiftUI animations
+//
+//  Created by Nethan on 27/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
